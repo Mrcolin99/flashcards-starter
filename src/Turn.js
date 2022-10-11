3580abc25d3a939bd3b03151dc7b2a7a19fb5c84
Mrcolin99 <colinciervo@gmail.com>
2022-10-11 12:31:18 -0400

    Add turn tests

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,21 @@
+const { prototype } = require("mocha")
+
 class Turn {
     constructor(guess, card) {
-
+        this.guess = guess
+        this.card = card
+    }
+    returnGuess() {
+        return this.guess
+    }
+    returnCard(id) {
+        const currentCard = prototypeData.filter((ele) => ele === id)
+        console.log(currentCard)
+        return currentCard
     }
 }
+
+
+
+
 module.exports = Turn
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -3,14 +3,32 @@ const expect = chai.expect;
 
 const Turn = require('../src/Turn');
 
-describe('Turn', function() {
+describe('Turn', () => {
     beforeEach(() => {
-        turn = new Turn()
+     turn = new Turn('frog', 1)
     })
-    it('should be a function', function() {
-        expect(turn).to.be.a('function')
+    it('should be a function', () => {
+        expect(Turn).to.be.a('function')
     })
-    it('should be an instance of a turn', function() {
+    it('should be an instance of a turn', () => {
         expect(turn).to.be.an.instanceof(Turn)
+    })
+    it('should contain a guess', () => {
+        expect(turn.guess).to.equal('frog')
+    })
+    it('should contain a card', () => {
+        expect(turn.card).to.equal(1)
+    })
+    it('should be able to return a guess', () => {
+        expect(turn.returnGuess()).to.equal('frog')
+    })
+    it('should return the current card', () => {
+        expect(turn.returnCard(1)).to.deep.equal({
+            "id": 1,
+            "question": "What allows you to define a set of related information using key-value pairs?",
+            "answers": ["object", "array", "function"],
+            "correctAnswer": "object"
+        })
+        turn.returnCard(1)
     })
 })
