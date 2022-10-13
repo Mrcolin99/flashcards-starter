07962141a8e0e5b9c4f96156a904126b8d69aa49
Colin Ciervo <colinciervo@@gmail.com>
2022-10-13 15:04:50 -0400

    Add tests and methods for the turn class

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,25 @@
 class Turn {
     constructor(guess, card) {
-
+        this.guess = guess
+        this.card = card
+        this.correct
+    }
+    returnGuess() {
+        return this.guess
+    }
+    returnCard() {
+        return this.card
+    }
+    evaluateGuess() {
+        if(this.guess === this.card.correctAnswer) {
+            this.correct = true
+            return true
+        }
+    }
+    giveFeedback() {
+        if(this.correct = true) {
+            return 'correct!'
+        } else {return 'incorrect!'}
     }
 }
 module.exports = Turn
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,12 +5,37 @@ const Turn = require('../src/Turn');
 
 describe('Turn', function() {
     beforeEach(() => {
-        turn = new Turn()
+        turn = new Turn('object', {
+            "id": 1,
+            "question": "What allows you to define a set of related information using key-value pairs?",
+            "answers": ["object", "array", "function"],
+            "correctAnswer": "object"
+          })
     })
     it('should be a function', function() {
         expect(turn).to.be.a('function')
     })
     it('should be an instance of a turn', function() {
         expect(turn).to.be.an.instanceof(Turn)
+    })
+    it('should be instantiated with a guess and a card', function() {
+        expect(turn.guess).to.be.a('string')
+    })
+    it('should be able to return a guess', function() {
+        expect(turn.returnGuess()).to.equal('object')
+    })
+    it('should be able to return the current card', function() {
+        expect(turn.returnCard()).to.deep.equal({
+            "id": 1,
+            "question": "What allows you to define a set of related information using key-value pairs?",
+            "answers": ["object", "array", "function"],
+            "correctAnswer": "object"
+          })
+    })
+    it('should evaluate the asnwer and return true or false', () => {
+        expect(turn.evaluateGuess()).to.equal(true)
+    })
+    it('should return with feedback if the answer is correct or incorrect', () => {
+        expect(turn.giveFeedback()).to.equal('correct!')
     })
 })
